4ea27a26c97ac640b7269f95e69c400ff4ce1e55
Urvin <urvinkirindongo@gmail.com>
2021-03-12 01:27:40 +0100

    Updating

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -3,6 +3,8 @@ package com.example.demo.api;
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,7 +22,7 @@ public class PersonController {
 		}
 
 		@PostMapping
-		public void addPerson(@RequestBody Person person){
+		public void addPerson(@NonNull @Validated @RequestBody Person person){
 				personService.addPerson(person);
 		}
 
@@ -41,7 +43,7 @@ public class PersonController {
 		}
 
 		@PutMapping(path = "{id}")
-		public void updatePersonById(@PathVariable("id") UUID id, @RequestBody Person personToUpdate){
+		public void updatePersonById(@PathVariable("id") UUID id, @NonNull @RequestBody Person personToUpdate){
 				personService.updatePerson(id, personToUpdate);
 		}
 }
